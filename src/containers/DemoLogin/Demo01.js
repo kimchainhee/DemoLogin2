72cbf098a4d3918e41e1ad307927bf4bedbe9d95
unknown <kusphan15@gmail.com>
2021-05-28 15:14:05 +0700

    Update LOGINHAHALOLO S3

--- src/containers/DemoLogin/Demo01.js
+++ src/containers/DemoLogin/Demo01.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Login from "../../components/login";
-import LoginHalo from '../../components/LoginHalo';
+import LOGIN from "../../components/LOGIN";
+import LOGINHalo from '../../components/LOGINHalo';
+import Simple01 from '../../components/SimpleBottomNavigation';
 
 function Demo2() {
     return (
       <div>
-        <LoginHalo/>
+        <LOGINHalo/>
       </div>
       );
   }
--- src/icons/Pages/GetAppIcon.js
+++ src/icons/Pages/GetAppIcon.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import GetAppIcon from '@material-ui/icons/GetApp';
+
 export default function IconExample() {
   return (
     <>
