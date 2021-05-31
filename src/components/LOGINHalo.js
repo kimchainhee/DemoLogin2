0a8d397cf137ffc4e39df0e8810855cf3dcb6cba
Minh Huy <tmh.pug@gmail.com>
2021-05-31 16:00:10 +0700

    Refactor page login

--- src/components/LOGINHalo.js
+++ src/components/LOGINHalo.js
@@ -1,268 +1,170 @@
-import React from "react";
-
-import AtomButton from "./Atomic/atoms/AtomButton/index";
-import AtomContainer from "./Atomic/atoms/AtomContainer/index";
-import AtomBox from "./Atomic/atoms/AtomBox/index";
-import AtomLink from "./Atomic/atoms/AtomLink/index";
-import AtomTextField from "./Atomic/atoms/AtomTextField/index";
-import AtomGrid from "./Atomic/atoms/AtomGrid/index";
-import AtomTypography from "./Atomic/atoms/AtomTypography/index";
-import AtomDivider from "./Atomic/atoms/AtomDivider/index";
-import AtomHidden from "./Atomic/atoms/AtomHidden/index";
-import { Card, CardActions } from "@material-ui/core";
-
-import PropTypes from "prop-types";
-
-import { makeStyles } from "@material-ui/core/styles";
-
-import AndroidIcon from "../icons/Pages/AndroidIcon";
-import AppleIcon from "../icons/Pages/AppleIcon";
-
-import ImgLogo from "../img/Pages/Logo";
-import { CardContent } from "@material-ui/core";
-import { getThemeProps } from "@material-ui/styles";
+import {
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  Link,
+  useMediaQuery,
+} from "@material-ui/core"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import React from "react"
+import AndroidIcon from "@material-ui/icons/Android"
+import AppleIcon from "@material-ui/icons/Apple"
+import ImgLogo from "../img/Pages/Logo"
+import AtomBox from "./Atomic/atoms/AtomBox/index"
+import AtomButton from "./Atomic/atoms/AtomButton/index"
+import AtomContainer from "./Atomic/atoms/AtomContainer/index"
+import AtomGrid from "./Atomic/atoms/AtomGrid/index"
+import AtomHidden from "./Atomic/atoms/AtomHidden/index"
+import AtomTextField from "./Atomic/atoms/AtomTextField/index"
+import AtomTypography from "./Atomic/atoms/AtomTypography/index"
 
 const useStyles = makeStyles((theme) => ({
   rootCard: {
     width: 400,
-    height: 390,
+    margin: "auto",
   },
-  rootBox: {
-    display: "flex",
-    marginTop: theme.spacing(8),
+  btnSignUp: {
+    backgroundColor: theme.palette.success.main,
+    color: theme.palette.common.white,
+    "&:hover": {
+      backgroundColor: theme.palette.success.dark,
+    },
   },
-  rootBox01: {
-    marginTop: theme.spacing(8),
-  },
-  rootBox02: {
-    height : '120px',
-    backgroundColor: "#eeeeee",
-  },
-  rootContent: {
-    fontWeight: "bold",
-  },
-  rootTextField01: {
-    width: "90%",
-  },
-  rootButton01: {
-    marginTop: theme.spacing(1),
-    width: "90%",
-    borderRadius: "24px",
-    textTransform: "none",
-  },
-  rootButton02: {
-    marginTop: theme.spacing(1),
-    borderRadius: "24px",
-    textTransform: "none",
-  },
-  rootButton03: {
-    marginTop: theme.spacing(4),
-    marginRight: theme.spacing(1),
-    borderRadius: "24px",
-    textTransform: "none",
-    width: "135px",
-  },
-  rootButton04: {
-    textTransform: "none",
-    fontSize: "12px",
-  },
-  rootItem01: {
-    color: "grey",
-    fontSize: "12px",
-  },
-  rootItem02: {
-    fontSize: "12px",
-  },
-  rootItem03: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(1),
-    fontSize: "12px",
-  },
-  rootItem05: {
-    marginTop: theme.spacing(1),
-    fontSize: "15px",
-    color: "#757575",
-  },
-}));
-
-LoginHahalolo.propTypes = {
-  width: PropTypes.oneOf(["lg", "md", "sm", "xl", "xs"]).isRequired,
-};
-
-export default function LoginHahalolo() {
-  const classes = useStyles();
+}))
+function InputForm() {
   return (
-    <AtomBox className={classes.rootBox} flexDirection='column' height='100vh'>
+    <AtomGrid container spacing={2}>
+      <AtomGrid item xs={12}>
+        <AtomTextField fullWidth label="Email hoặc số điện thoại" />
+      </AtomGrid>
+      <AtomGrid item xs={12}>
+        <AtomTextField fullWidth label="Mật khẩu" />
+      </AtomGrid>
+    </AtomGrid>
+  )
+}
+function ActionsForm() {
+  const classes = useStyles()
+  return (
+    <Grid container spacing={2} justify="center">
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" fullWidth disableElevation>
+          Đăng nhập
+        </Button>
+      </Grid>
+      <Grid item>
+        <Link href="#">
+          <b>Quên mật khẩu?</b>
+        </Link>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs>
+            <Divider />
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary">hoặc</Typography>
+          </Grid>
+          <Grid item xs>
+            <Divider />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Button className={classes.btnSignUp}>Đăng ký</Button>
+      </Grid>
+    </Grid>
+  )
+}
+export default function LoginHahalolo() {
+  const classes = useStyles()
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down("sm"))
+  return (
+    <AtomBox
+      display="flex"
+      paddingTop={8}
+      flexDirection="column"
+      height="100vh"
+    >
       <AtomContainer>
-        <AtomGrid container spacing={6} justify="space-around">
-          <AtomGrid item md="true">
+        <AtomGrid container justify="center" spacing={3}>
+          <AtomGrid item xs="auto" md>
             <ImgLogo />
             <AtomHidden smDown>
-              <AtomBox className={classes.rootBox01}>
-                <AtomGrid container direction="column">
-                  <AtomGrid item xs={4}>
-                    <AtomTypography>Do you like</AtomTypography>
-                    <AtomTypography variant="h3">travelling?</AtomTypography>
-                    <AtomTypography>
-                      Do you want to know more about next destination?
-                    </AtomTypography>
-                    <AtomTypography className={classes.rootItem05}>
-                      Sign in with quick operation to experience and explore our
-                      awesome conveniences.
-                    </AtomTypography>
-                  </AtomGrid>
-                </AtomGrid>
+              <AtomBox pt={8} maxWidth={320}>
+                <AtomTypography variant="h5" gutterBottom>
+                  Do you like
+                </AtomTypography>
+                <AtomTypography variant="h3">travelling?</AtomTypography>
+                <AtomTypography variant="h5" paragraph>
+                  Do you want to know more about next destination?
+                </AtomTypography>
+                <AtomTypography variant="h6" color="textSecondary">
+                  Sign in with quick operation to experience and explore our
+                  awesome conveniences.
+                </AtomTypography>
               </AtomBox>
             </AtomHidden>
           </AtomGrid>
-          <AtomGrid item md="auto">
+          <AtomGrid item xs={12} md="auto">
             <Card className={classes.rootCard}>
               <CardContent>
-                <AtomTypography
-                  variant="h6"
-                  align="center"
-                  className={classes.rootContent}
-                >
-                  Đăng nhập
-                </AtomTypography>
+                <AtomGrid container spacing={3}>
+                  <AtomGrid item xs={12}>
+                    <AtomTypography variant="h5" align="center" gutterBottom>
+                      <b>Đăng nhập</b>
+                    </AtomTypography>
+                  </AtomGrid>
+                  <AtomGrid item xs={12}>
+                    <InputForm />
+                  </AtomGrid>
+                  <AtomGrid item xs={12}>
+                    <ActionsForm />
+                  </AtomGrid>
+                </AtomGrid>
               </CardContent>
-              <CardActions>
-                <AtomBox display="flex" justifyContent="center" width="100%">
-                  <AtomTextField
-                    id="email"
-                    label="Email hoặc số điện thoại"
-                    className={classes.rootTextField01}
-                  />
-                </AtomBox>
-              </CardActions>
-              <CardActions>
-                <AtomBox display="flex" justifyContent="center" width="100%">
-                  <AtomTextField
-                    id="password"
-                    label="Mật khẩu"
-                    className={classes.rootTextField01}
-                  />
-                </AtomBox>
-              </CardActions>
-              <CardActions>
-                <AtomBox display="flex" justifyContent="center" width="100%">
-                  <AtomButton
-                    type="login"
-                    fullWidth
-                    variant="contained"
-                    size="large"
-                    color="primary"
-                    className={classes.rootButton01}
-                  >
-                    Đăng nhập
-                  </AtomButton>
-                </AtomBox>
-              </CardActions>
-              <CardActions>
-                <AtomBox display="flex" justifyContent="center" width="100%">
-                  <AtomLink
-                    href="https://www.hahalolo.com/auth/forgotpw"
-                    variant="body2"
-                  >
-                    Quên mật khẩu?
-                  </AtomLink>
-                </AtomBox>
-              </CardActions>
-              <AtomGrid
-                container
-                justify="center"
-                spacing={2}
-                alignItems="center"
-              >
-                <AtomGrid item xs={5}>
-                  <AtomDivider />
-                </AtomGrid>
-                <AtomGrid item>
-                  <AtomTypography className={classes.rootItem01}>
-                    hoặc
-                  </AtomTypography>
-                </AtomGrid>
-                <AtomGrid item xs={5}>
-                  <AtomDivider />
-                </AtomGrid>
-              </AtomGrid>
-              <CardActions>
-                <AtomBox display="flex" justifyContent="center" width="100%">
-                  <AtomButton
-                    type="registration"
-                    variant="contained"
-                    size="large"
-                    color="primary"
-                    className={classes.rootButton02}
-                  >
-                    Đăng ký
-                  </AtomButton>
-                </AtomBox>
-              </CardActions>
             </Card>
           </AtomGrid>
         </AtomGrid>
       </AtomContainer>
-      <AtomBox className={classes.rootBox02} mt='auto' >
+      <AtomBox bgcolor="#eee" mt="auto" py={5}>
         <AtomContainer>
-          <AtomGrid container justify="space-around" alignItems="center" >
-            <AtomGrid item md="true">
-              <AtomHidden smDown>
-                <AtomButton
-                  type="downloadGP"
-                  variant="outlined"
-                  size="large"
-                  href="https://play.google.com/store/apps/details?id=com.hahalolo.android.social&hl=en"
-                  className={classes.rootButton03}
-                >
-                  <AndroidIcon />
-                  <AtomTypography className={classes.rootItem02}>
-                    Google play
-                  </AtomTypography>
-                </AtomButton>
-                <AtomButton
-                  type="downloadAT"
-                  variant="outlined"
-                  size="large"
-                  href="https://apps.apple.com/us/app/hahalolo/id1437417120"
-                  className={classes.rootButton03}
-                >
-                  <AppleIcon />
-                  <AtomTypography className={classes.rootItem02}>
-                    Apple store
-                  </AtomTypography>
-                </AtomButton>
-              </AtomHidden>
-              <AtomTypography className={classes.rootItem03}>
-                © 2017 Hahalolo. All rights reserved
-              </AtomTypography>
+          <AtomGrid container alignItems="center" justify="center">
+            <AtomGrid item xs={12} md>
+              <Grid
+                container
+                spacing={1}
+                justify={matches ? "center" : undefined}
+              >
+                <Grid item>
+                  <Button startIcon={<AndroidIcon />} variant="outlined">
+                    Google Play
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button startIcon={<AppleIcon />} variant="outlined">
+                    App Store
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography align={matches ? "center" : undefined}>
+                    © 2017 Hahalolo. All rights reserved
+                  </Typography>
+                </Grid>
+              </Grid>
             </AtomGrid>
-            <AtomGrid item xs={12} md="auto" >
-              <AtomGrid container justify="center" >
-                <AtomGrid item>
-                <AtomButton
-                  href="vn"
-                  type="vietnam"
-                  className={classes.rootButton04}
-                >
-                  Tiếng Việt
-                </AtomButton>
-                </AtomGrid>
-                    <AtomDivider orientation="vertical" flexItem />
-                <AtomGrid item>
-                <AtomButton
-                  href="en"
-                  type="english"
-                  className={classes.rootButton04}
-                >
-                  English
-                </AtomButton>
-                </AtomGrid>
-              </AtomGrid>
+            <AtomGrid item>
+              <AtomButton>Tiếng Việt</AtomButton>
+              &nbsp;|&nbsp;
+              <AtomButton>English</AtomButton>
             </AtomGrid>
           </AtomGrid>
         </AtomContainer>
       </AtomBox>
     </AtomBox>
-  );
+  )
 }
